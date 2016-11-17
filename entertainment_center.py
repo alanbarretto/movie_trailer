237069f3ada9848e8352790362a5e396949d934f
Francis Barretto <fbarretto@fbarretto-macbookpro.roam.corp.google.com>
2016-11-16 17:45:18 -0800

    Remove links to posters

--- entertainment_center.py
+++ entertainment_center.py
@@ -12,14 +12,14 @@ contact = media.Movie("Contact",
 
 when_harry_met_sally = media.Movie("When Harry Met Sally",
                                    "Can men and women be just friends?",
-                                   "/users/fbarretto/documents/whenharrymetsally.jpg",
+                                   "",
                                    "https://www.youtube.com/watch?v=V8DgDmUHVto")
 
 the_black_stallion = media.Movie("The Black Stallion",
-                                   "A boy and a horse get stranded on an island "
-                                   "and become best friends.",
-                                   "/users/fbarretto/documents/black-stallion.jpg",
-                                   "https://www.youtube.com/watch?v=ORFWdXl_zJ4")
+                                 "A boy and a horse get stranded on an island "
+                                 "and become best friends.",
+                                 "",
+                                 "https://www.youtube.com/watch?v=ORFWdXl_zJ4")
 up = media.Movie("Up",
                 "An old man, a boy, and a flying house",
                 "",
@@ -27,12 +27,12 @@ up = media.Movie("Up",
 
 ferris_buellers_day_off = media.Movie("Ferris Bueller's Day Off",
                                    "A high school kid plays hookie.",
-                                   "/users/fbarretto/documents/ferrisbueller.jpg",
+                                   "",
                                    "https://www.youtube.com/watch?v=D6gABQFR94U")
 
 wreck_it_ralph = media.Movie("Wreck It Ralph",
                              "A video game antagonist dreams of becoming the hero.",
-                             "/users/fbarretto/documents/wreckitralph.jpg",
+                             "",
                              "https://www.youtube.com/watch?v=87E6N7ToCxs")
 
 you_me_and_dupree = media.Movie("You, Me, and Dupree",
